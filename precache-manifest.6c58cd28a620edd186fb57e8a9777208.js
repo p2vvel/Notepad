27399d277ce4c05c48e07703526d5d8a6cf801df
pawel <p2vvel@gmail.com>
2020-09-12 21:18:10 +0200

    Updates

---
+++ precache-manifest.6c58cd28a620edd186fb57e8a9777208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a69be45f44c80414f305280d291d42",
+    "url": "/Notepad/index.html"
+  },
+  {
+    "revision": "f9f10bafbc09243ccb71",
+    "url": "/Notepad/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2935517a0d2eafb591e2",
+    "url": "/Notepad/static/css/main.97cdbac2.chunk.css"
+  },
+  {
+    "revision": "f9f10bafbc09243ccb71",
+    "url": "/Notepad/static/js/2.fe67d1af.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Notepad/static/js/2.fe67d1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2935517a0d2eafb591e2",
+    "url": "/Notepad/static/js/main.a3cbf56a.chunk.js"
+  },
+  {
+    "revision": "e2c116ff8c09ff6009bd",
+    "url": "/Notepad/static/js/runtime-main.61337d1a.js"
+  }
+]);
